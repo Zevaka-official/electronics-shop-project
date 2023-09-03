03db5243526d354fe4c26f16a1d64e8575dbf337
Adam <A.ergashev@internet.ru>
2023-09-03 11:35:54 +0300

    HomeWork_1
    - add tests
    - refactor class Item

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,15 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+from src.item import Item
+
+
+def test_calculate_total_price():
+    item = Item("Смартфон", 10000, 20)
+    assert item.calculate_total_price() == 200000
+
+
+def test_apply_discount():
+    item = Item("Смартфон", 10000, 20)
+    Item.discount = 0.8
+    item.apply_discount()
+    assert item.price == 10000.0
